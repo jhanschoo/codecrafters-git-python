d25f3d7bc41b8f9b489c195f12a859dbf95e93bf
Johannes Choo <jhanschoo@gmail.com>
2024-11-19 10:17:39 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,24 +1,36 @@
-import sys
+import argparse
+import zlib
 import os
 
+def init(**_kwargs) -> None:
+    os.mkdir(".git")
+    os.mkdir(".git/objects")
+    os.mkdir(".git/refs")
+    with open(".git/HEAD", "w") as f:
+        f.write("ref: refs/heads/main\n")
+    print("Initialized git directory")
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
+def cat_file(p : str, **_kwargs) -> None:
+    prefix, rest = p[:2], p[2:]
+    with open(os.path.join(".git", "objects", prefix, rest), "rb") as f:
+        raw = zlib.decompress(f.read())
+        content = raw.decode()
+        metadata, content = content.split("\0", 1)
+        print(content, end="")
 
-    # Uncomment this block to pass the first stage
-    
-    command = sys.argv[1]
-    if command == "init":
-        os.mkdir(".git")
-        os.mkdir(".git/objects")
-        os.mkdir(".git/refs")
-        with open(".git/HEAD", "w") as f:
-            f.write("ref: refs/heads/main\n")
-        print("Initialized git directory")
-    else:
-        raise RuntimeError(f"Unknown command #{command}")
+parser = argparse.ArgumentParser()
 
+subparsers = parser.add_subparsers(dest="command", title="subcommands", required=True)
+parser_init = subparsers.add_parser("init")
+parser_init.set_defaults(func=init)
+
+parser_cat_file = subparsers.add_parser("cat-file")
+parser_cat_file.set_defaults(func=cat_file)
+parser_cat_file.add_argument("-p", required=True)
+
+def main() -> None:
+    args = parser.parse_args()
+    args.func(**vars(args))
 
 if __name__ == "__main__":
     main()
